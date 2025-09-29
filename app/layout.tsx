20c3df22bbe119c00f8e44f2a543b91dab873200
nurtius <nullbsphantom@gmail.com>
2025-09-28 21:18:20 -0300

    terceiro commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Programa Avançado de 21 dias',
+  description: 'menopausa tranquila',
   generator: 'v0.app',
 }
 
